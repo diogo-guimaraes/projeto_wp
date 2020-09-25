0e5d6edd456919f6399ca538e19f361f0c832fc9
Diogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-09-25 17:56:52 -0300

    refactor(includes): custom-posts
    * corrige errors de sintaxe

--- wp-content/themes/fataltheme/inc/custom_posts.php
+++ wp-content/themes/fataltheme/inc/custom_posts.php
@@ -1,37 +1,37 @@
 <?php
-add_action('init', 'criar_post_postagens');
+
+add_action( 'init', 'criar_post_postagens');
 
 function criar_post_postagens(){
-  $labels = array(
-    'name' => _x('Postagens', 'post type general name'),
-    'singular_name' => _x('Postagem', 'post type singular name'),
-    'add_new' => _x('Adicionar novo', 'Novo Post'),
-    'add_new_item' => __('Novo Post'),
-    'edit_item' => __('Editar Post'),
-    'new_item' => __('Novo Post'),
-    'search_items' => __('Procurar Post'),
-    'not_found' => __('Nenhuma postagem encontrada'),
-    'not_found_in_trash' => __('Nenhuma postagem na lixeira'),
-    'menu_name' => 'Postagens'
-  );
+    $labels = array(
+        'name' => _x( 'Postagens', 'post type general name'),
+        'singular_name' => _x('Postagem', 'post type singular name'),
+        'add_new' => _x('Adicionar novo', 'Novo Post'),
+        'add_new_item' => __('Novo Post'),
+        'edit_item' => __('Editar Post'),
+        'new_item' => __('Novo Post'),
+        'view_item' => __('Ver Post'),
+        'search_items' => __('Procurar Posts'),
+        'not_found' => __('Nenhuma Postagem encontrada'),
+        'note_found_in_trash' => __('Nenhuma Postagem na Lixeira'),
+        'menu_name' => 'Postagens'
+    );
 
-  $args = array(
-    'label' => $labels,
-    'public' => true,
-    'public_queryable' => true,
-    'show_ui'=> true,
-    'query_var' => true,
-    'rewrite' => true,
-    'capability_type' => 'post',
-    'has_archive' => true,
-    'hierarchical' => false,
-    'menu_position' => 'top',
-    'register_meta_box_cb' => 'postagens_meta_box',
-    'supports' => array('title', 'editor', 'thumbnail'),
-    'show_in_rest' => true
-  );
+    $args = array(
+        'labels' => $labels,
+        'public' => true,
+        'public_queryable' => true,
+        'show_ui' => true,
+        'query_var' => true,
+        'rewrite' => true,
+        'capability_type' => 'post',
+        'has_archive' => true,
+        'hierarchical' => false,
+        'menu_position' => 'top',
+        'register_meta_box_cb' => 'postagens_meta_box',
+        'supports' => array('title', 'editor', 'thumbnail'),
+        'show_in_rest' => true
+    );
 
-  register_post_type('postagens', $args);
-  
-
+register_post_type( 'postagens', $args );
 }
